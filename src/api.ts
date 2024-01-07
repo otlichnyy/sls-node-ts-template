20551f540d062ce4f0a59aee0ad98b55267ed85f
otlichnyy <12508523+otlichnyy@users.noreply.github.com>
2024-01-07 20:49:37 +0530

    feat: :sparkles: add local dynamodb setup

--- src/api.ts
+++ src/api.ts
@@ -13,6 +13,35 @@ app.get("/env", (req, res) => {
     res.send({ myEnv: process.env.DB_URL || "unable to find" });
 });
 
+//--------------
+//--------------
+
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({
+    region: "localhost",
+    endpoint: "http://0.0.0.0:8000"
+});
+const docClient = DynamoDBDocumentClient.from(client);
+
+app.get("/db", async (req, res) => {
+    const command = new GetCommand({
+        TableName: "demotable",
+        Key: {
+            PK: "PROJECT#1",
+            SK: "PROJECT#1"
+        }
+    });
+
+    const response = await docClient.send(command);
+    console.log(response);
+    res.json(JSON.stringify(response.Item));
+});
+
+//--------------
+//--------------
+
 app.get("/error", (req, res) => {
     throw new Error("MEh!");
     res.status(200).send({ status: "OK" });
